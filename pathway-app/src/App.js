0897dbbde41d1c1ed581fb8bb87ff961c7fae583
Rohit Musti <rmusti@americanforests.org>
2021-01-12 10:23:31 -0500

    making it Reactive to size of screen onload

--- pathway-app/src/App.js
+++ pathway-app/src/App.js
@@ -5,7 +5,21 @@ import { Component, Fragment } from "react";
 import locationPin from "./data/betterIcon.svg";
 import locationPin2 from "./data/betterIcon2.svg";
 import L, { point } from "leaflet";
-import { Button, Drawer, Col, Row, Modal } from "antd";
+import {
+  Space,
+  Button,
+  Drawer,
+  Col,
+  Row,
+  Modal,
+  Card,
+  Tabs,
+  Select,
+} from "antd";
+
+const { Option } = Select;
+const { TabPane } = Tabs;
+const smolWindow = 855;
 
 const myIcon = L.icon({
   iconUrl: locationPin,
@@ -66,6 +80,24 @@ class App extends Component {
     });
   };
 
+  onSelection(value) {
+    console.log(this.state.data[value]);
+    const pointOfInterest = {
+      key: value,
+      ...this.state.data[value],
+    };
+    const newLandingLocation = {
+      lat: pointOfInterest.Lat,
+      lng: pointOfInterest.Lon,
+      zoom: 12,
+    };
+    this.setState({
+      landingLocation: newLandingLocation,
+      currentPointOfInterest: pointOfInterest,
+      jobinfovisible: true,
+    });
+  }
+
   showJobInfoDrawer = () => {
     this.setState({
       jobinfovisible: true,
@@ -95,138 +127,241 @@ class App extends Component {
       this.state.landingLocation.lng,
     ];
 
+    console.log("inner width", window.innerWidth);
+
     return (
       <Fragment>
         <Nav onClick={() => this.showInstructionsDrawer()} />
 
-        <Drawer
+        <Modal
           title="Instructions"
-          height={350}
-          onClose={this.onInstructionsDrawerClose}
           visible={this.state.instructionsVisbile}
-          placement={"bottom"}
-          footer={
-            <div
-              style={{
-                textAlign: "right",
-              }}
-            >
-              <Button onClick={this.onInstructionsDrawerClose} type="primary">
-                Close
-              </Button>
-            </div>
-          }
+          onOk={this.onInstructionsDrawerClose}
         >
           <Row gutter={16}>
-            <Col span={12}>
-              <b>How to Look at Individual Jobs: </b> <br />
-              <p>
-                Please being by zoom in and out of various parts of the map to
-                view to see the different locations of each career pathway.
-              </p>
-              <p>
-                When you feel like are ready to look at a particular job
-                location more closely, click on the icon associated with that
-                location.
-              </p>
-              <p>
-                This icon will turn red, indicating that this is the location
-                you are currently viewing more information for. A scrollabe
-                panel will pop up containing a description for that particular
-                location. After you have finished reading the description and
-                you are ready to see the next job site, click the close button
-                and continue exploring!
-              </p>
-              <p>
-                <i>Note:</i> You cannot continue exploring the map until after
-                you have closed the information panel for that particular job.
-              </p>
-            </Col>
+            <p>
+              <b>Welcome to the Career Pathways Dashboard!</b>
+            </p>
+            <p>
+              Please being by zoom in and out of various parts of the map to
+              view to see the different locations of each career pathway.
+            </p>
+            <p>
+              When you feel like are ready to look at a particular job location
+              more closely, click on the icon associated with that location.
+            </p>
+            <p>
+              This icon will turn red, indicating that this is the location you
+              are currently viewing more information for. A scrollabe panel will
+              pop up containing a description for that particular location.
+              After you have finished reading the description and you are ready
+              to see the next job site, click the close button and continue
+              exploring!
+            </p>
+            <p>
+              <i>Note:</i> You cannot continue exploring the map until after you
+              have closed the information panel for that particular job.
+            </p>
           </Row>
-        </Drawer>
+        </Modal>
 
-        <Drawer
-          title="Job Information Panel"
-          height={300}
-          onClose={this.onJobInfoClose}
-          visible={this.state.jobinfovisible}
-          placement={"bottom"}
-          footer={
-            <div
+        {window.innerWidth < smolWindow && (
+          <Fragment>
+            <Drawer
+              title="Job Information Panel"
+              height={300}
+              onClose={this.onJobInfoClose}
+              visible={this.state.jobinfovisible}
+              placement={"bottom"}
+              footer={
+                <div
+                  style={{
+                    textAlign: "right",
+                  }}
+                >
+                  <Button onClick={this.onJobInfoClose} type="primary">
+                    Close
+                  </Button>
+                </div>
+              }
+            >
+              <Row gutter={16}>
+                <Col span={12}>
+                  <b>Organization:</b>{" "}
+                  {(this.state.currentPointOfInterest &&
+                    this.state.currentPointOfInterest["Org"]) ||
+                    "No Org Available"}
+                </Col>
+              </Row>
+              <Row gutter={16}>
+                <Col span={24}>
+                  <b>Program: </b>
+                  {(this.state.currentPointOfInterest &&
+                    this.state.currentPointOfInterest["Program"]) ||
+                    "No Program Available"}
+                </Col>
+              </Row>
+              <Row gutter={16}>
+                <Col span={24}>
+                  <b>Description: </b>
+                  {(this.state.currentPointOfInterest &&
+                    this.state.currentPointOfInterest["additional.notes"]) ||
+                    "No Description Available"}
+                </Col>
+              </Row>
+              <Row gutter={16}>
+                <Col span={24}>
+                  <b>City, State: </b>
+                  {(this.state.currentPointOfInterest &&
+                    this.state.currentPointOfInterest["City"]) ||
+                    "No City Available"}
+                  {", "}
+                  {(this.state.currentPointOfInterest &&
+                    this.state.currentPointOfInterest["State"]) ||
+                    "No State Available"}
+                </Col>
+              </Row>
+              <Row gutter={16}>
+                <Col span={12}>
+                  <b>Notes: </b>
+                  {(this.state.currentPointOfInterest &&
+                    this.state.currentPointOfInterest["otherNotes"]) ||
+                    "No Notes Available"}
+                </Col>
+              </Row>
+              <Row gutter={16}>
+                <Col span={24}>
+                  {(this.state.currentPointOfInterest &&
+                    this.state.currentPointOfInterest["link"] && (
+                      <b>
+                        <a
+                          href={
+                            this.state.currentPointOfInterest &&
+                            this.state.currentPointOfInterest["link"]
+                          }
+                          target="_blank"
+                        >
+                          Click Here to Learn More
+                        </a>
+                      </b>
+                    )) ||
+                    "No link available"}
+                </Col>
+              </Row>
+            </Drawer>
+          </Fragment>
+        )}
+
+        {window.innerWidth >= smolWindow && (
+          <Fragment>
+            <Card
+              title="Career Pathways Dashboard"
               style={{
-                textAlign: "right",
+                zIndex: 500,
+                width: "35%",
+                right: 25,
+                top: 50,
+                backgroundColor: "#FFFFFF",
+                position: "absolute",
+                maxHeight: "800px",
               }}
             >
-              <Button onClick={this.onJobInfoClose} type="primary">
-                Close
-              </Button>
-            </div>
-          }
-        >
-          <Row gutter={16}>
-            <Col span={12}>
-              <b>Organization:</b>{" "}
-              {(this.state.currentPointOfInterest &&
-                this.state.currentPointOfInterest["Org"]) ||
-                "No Org Available"}
-            </Col>
-          </Row>
-          <Row gutter={16}>
-            <Col span={24}>
-              <b>Program: </b>
-              {(this.state.currentPointOfInterest &&
-                this.state.currentPointOfInterest["Program"]) ||
-                "No Program Available"}
-            </Col>
-          </Row>
-          <Row gutter={16}>
-            <Col span={24}>
-              <b>Description: </b>
-              {(this.state.currentPointOfInterest &&
-                this.state.currentPointOfInterest["additional.notes"]) ||
-                "No Description Available"}
-            </Col>
-          </Row>
-          <Row gutter={16}>
-            <Col span={24}>
-              <b>City, State: </b>
-              {(this.state.currentPointOfInterest &&
-                this.state.currentPointOfInterest["City"]) ||
-                "No City Available"}
-              {", "}
-              {(this.state.currentPointOfInterest &&
-                this.state.currentPointOfInterest["State"]) ||
-                "No State Available"}
-            </Col>
-          </Row>
-          <Row gutter={16}>
-            <Col span={12}>
-              <b>Notes: </b>
-              {(this.state.currentPointOfInterest &&
-                this.state.currentPointOfInterest["otherNotes"]) ||
-                "No Notes Available"}
-            </Col>
-          </Row>
-          <Row gutter={16}>
-            <Col span={24}>
-              {(this.state.currentPointOfInterest &&
-                this.state.currentPointOfInterest["link"] && (
-                  <b>
-                    <a
-                      href={
-                        this.state.currentPointOfInterest &&
-                        this.state.currentPointOfInterest["link"]
+              <div>
+                <Row gutter={16}>
+                  <Select
+                    showSearch
+                    style={{ width: 200 }}
+                    placeholder="Select An Organization"
+                    optionFilterProp="children"
+                    onChange={this.onSelection.bind(this)}
+                    filterOption={(input, option) =>
+                      option.children
+                        .toLowerCase()
+                        .indexOf(input.toLowerCase()) >= 0
+                    }
+                  >
+                    {Object.keys(this.state.data).map((key, index) => {
+                      let currentSpot = this.state.data[key];
+
+                      if (currentSpot.Lat) {
+                        return (
+                          <Option value={key}>{currentSpot["Org"]}</Option>
+                        );
                       }
-                      target="_blank"
-                    >
-                      Click Here to Learn More
-                    </a>
-                  </b>
-                )) ||
-                "No link available"}
-            </Col>
-          </Row>
-        </Drawer>
+                    })}
+                  </Select>
+                </Row>
+                <Space direction="vertical" size="100" />
+                <Row gutter={16}>
+                  <p>
+                    {this.state.currentPointOfInterest && <b>Organization: </b>}
+                    {this.state.currentPointOfInterest && (
+                        <b>Organization: </b>
+                      ) &&
+                      (this.state.currentPointOfInterest["Org"] ||
+                        "No Org Available")}
+                  </p>
+                </Row>
+                <Row gutter={16}>
+                  <p>
+                    {this.state.currentPointOfInterest && <b>Program: </b>}
+                    {this.state.currentPointOfInterest && <b>Program: </b> &&
+                      (this.state.currentPointOfInterest["Program"] ||
+                        "No Program Available")}
+                  </p>
+                </Row>
+                <Row gutter={16}>
+                  <p>
+                    {this.state.currentPointOfInterest && <b>Description: </b>}
+                    {this.state.currentPointOfInterest && (
+                        <b>Description: </b>
+                      ) &&
+                      (this.state.currentPointOfInterest["additional.notes"] ||
+                        "No Description Available")}
+                  </p>
+                </Row>
+                <Row gutter={16}>
+                  <p>
+                    {this.state.currentPointOfInterest && <b>City, State: </b>}
+                    {this.state.currentPointOfInterest && (
+                        <b>City, State: </b>
+                      ) &&
+                      (this.state.currentPointOfInterest["City"] ||
+                        "No City Available") &&
+                      ", " &&
+                      (this.state.currentPointOfInterest["State"] ||
+                        "No State Available")}
+                  </p>
+                </Row>
+                <Row gutter={16}>
+                  <p>
+                    {this.state.currentPointOfInterest && <b>Notes: </b>}
+                    {this.state.currentPointOfInterest && <b>Notes: </b> &&
+                      (this.state.currentPointOfInterest["otherNotes"] ||
+                        "No Notes Available")}
+                  </p>
+                </Row>
+                <Row gutter={16}>
+                  {this.state.currentPointOfInterest &&
+                    ((this.state.currentPointOfInterest["link"] && (
+                      <b>
+                        <a
+                          href={
+                            this.state.currentPointOfInterest &&
+                            this.state.currentPointOfInterest["link"]
+                          }
+                          target="_blank"
+                        >
+                          Click Here to Learn More
+                        </a>
+                      </b>
+                    )) ||
+                      "No link available")}
+                </Row>
+              </div>
+            </Card>
+          </Fragment>
+        )}
 
         <Map
           className="map"
